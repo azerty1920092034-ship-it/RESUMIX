687255a85c7df30f1e88242861d619a7f356dd74
anime king <azerty1920092034@gmail.com>
2026-03-30 00:15:13 +0100

    Fix Google OAuth duplicate email

--- backend/passport.js
+++ backend/passport.js
@@ -13,16 +13,23 @@ module.exports = (passport) => {
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ googleId: profile.id });
-      if (!user) {
-        user = await User.create({
-          googleId: profile.id,
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          avatar: profile.photos[0].value,
-          firstVisit: new Date(),
-          trialEnds: new Date(Date.now() + 24 * 60 * 60 * 1000),
-        });
-      }
+if (!user) {
+  user = await User.findOne({ email: profile.emails[0].value });
+  if (user) {
+    user.googleId = profile.id;
+    user.avatar = user.avatar || profile.photos[0].value;
+    await user.save();
+  } else {
+    user = await User.create({
+      googleId: profile.id,
+      name: profile.displayName,
+      email: profile.emails[0].value,
+      avatar: profile.photos[0].value,
+      firstVisit: new Date(),
+      trialEnds: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    });
+  }
+}
       return done(null, user);
     } catch (err) {
       return done(err, null);
