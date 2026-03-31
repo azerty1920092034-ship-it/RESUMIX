4e001d2d5437ed69d0bc16367284d3063f6e33e0
anime king <azerty1920092034@gmail.com>
2026-03-31 12:58:53 +0100

    Fix Google OAuth callback URL

--- backend/passport.js
+++ backend/passport.js
@@ -9,27 +9,27 @@ module.exports = (passport) => {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://resumix-1f.onrender.com/auth/google/callback"
+    callbackURL: "https://resumix-1-pmv0.onrender.com/auth/google/callback"
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ googleId: profile.id });
-if (!user) {
-  user = await User.findOne({ email: profile.emails[0].value });
-  if (user) {
-    user.googleId = profile.id;
-    user.avatar = user.avatar || profile.photos[0].value;
-    await user.save();
-  } else {
-    user = await User.create({
-      googleId: profile.id,
-      name: profile.displayName,
-      email: profile.emails[0].value,
-      avatar: profile.photos[0].value,
-      firstVisit: new Date(),
-      trialEnds: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    });
-  }
-}
+      if (!user) {
+        user = await User.findOne({ email: profile.emails[0].value });
+        if (user) {
+          user.googleId = profile.id;
+          user.avatar = user.avatar || profile.photos[0].value;
+          await user.save();
+        } else {
+          user = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value,
+            firstVisit: new Date(),
+            trialEnds: new Date(Date.now() + 24 * 60 * 60 * 1000),
+          });
+        }
+      }
       return done(null, user);
     } catch (err) {
       return done(err, null);
